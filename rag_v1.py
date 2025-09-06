57da1d3c0f48874e9837675c35c95810f3cc3222
subhadip07 <subhadipbag18@outlook.com>
2025-09-06 13:19:31 +0530

    add: rag_v1.py

--- rag_v1.py
+++ rag_v1.py
@@ -4,15 +4,18 @@ import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_groq import ChatGroq
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
 from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-load_dotenv()  # expects OPENAI_API_KEY in .env
+load_dotenv()  # expects GROQ_API_KEY in .env
+os.environ['LANGCHAIN_PROJECT'] = 'RAG Chatbot'
 
 PDF_PATH = "islr.pdf"  # <-- change to your PDF filename
+
 
 # 1) Load PDF
 loader = PyPDFLoader(PDF_PATH)
@@ -23,7 +26,7 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 splits = splitter.split_documents(docs)
 
 # 3) Embed + index
-emb = OpenAIEmbeddings(model="text-embedding-3-small")
+emb = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 vs = FAISS.from_documents(splits, emb)
 retriever = vs.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
@@ -34,7 +37,7 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 
 # 5) Chain
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+llm = ChatGroq(model="openai/gpt-oss-120b", temperature=0.7)
 def format_docs(docs): return "\n\n".join(d.page_content for d in docs)
 
 parallel = RunnableParallel({
